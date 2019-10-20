4a893b9843829fc77a509632e224d6623567a072
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-20 11:26:42 +0000

    Deploy kaustubh03/pokedex to github.com/kaustubh03/pokedex.git:gh-pages

---
+++ precache-manifest.8f5fc6924eb1a9160b31957969c970cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b1f71b7c770c8253e7",
+    "url": "/static/css/main.66144f74.chunk.css"
+  },
+  {
+    "revision": "35b1f71b7c770c8253e7",
+    "url": "/static/js/main.35b1f71b.chunk.js"
+  },
+  {
+    "revision": "5fe126ab7663c6072dc5",
+    "url": "/static/css/pokedex_app.f99c803e.chunk.css"
+  },
+  {
+    "revision": "5fe126ab7663c6072dc5",
+    "url": "/static/js/pokedex_app.5fe126ab.chunk.js"
+  },
+  {
+    "revision": "88f76e1feaea45ae1d1b",
+    "url": "/static/css/pokedex_home.0072f677.chunk.css"
+  },
+  {
+    "revision": "88f76e1feaea45ae1d1b",
+    "url": "/static/js/pokedex_home.88f76e1f.chunk.js"
+  },
+  {
+    "revision": "d851e22f56295528fb73",
+    "url": "/static/js/3.d851e22f.chunk.js"
+  },
+  {
+    "revision": "08a71a3ea8fea34a71e2",
+    "url": "/static/js/4.08a71a3e.chunk.js"
+  },
+  {
+    "revision": "032717163e2077b2b33f",
+    "url": "/static/js/runtime~main.03271716.js"
+  },
+  {
+    "revision": "f97f5c6510994f677877b08320475008",
+    "url": "/static/media/pokeball.f97f5c65.gif"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "/static/media/banner.49255cb2.png"
+  },
+  {
+    "revision": "881963246f5ff71ea5acc52ee0eaf9d2",
+    "url": "/static/media/noimage.88196324.png"
+  },
+  {
+    "revision": "6d347d0cccf943ddc8a046c25bc532de",
+    "url": "/index.html"
+  }
+];
